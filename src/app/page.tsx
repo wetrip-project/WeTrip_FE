105008ec76dbed9553f23871ec8d392c939a2491
HANTAEDONG <htd0913@gmail.com>
2025-05-25 13:12:11 +0900

    feat: [#5] 예시로 page.tsx 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import PhotoIcon from '@/assets/16_photo.svg'
+import Icon from '@/components/Icon/icon'
 
 export default function Home() {
   return (
     <div>
       <p className='font-26b bg-main2'>프리텐다드 26B</p>
       <p className='font-14r'>프리텐다드 20R</p>
-      <PhotoIcon />
+      <Icon iconName={'Photo'} />
     </div>
   )
 }
