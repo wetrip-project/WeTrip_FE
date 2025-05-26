30ede3ffca2278965ca26cbe177ab203c457a5e8
HANTAEDONG <htd0913@gmail.com>
2025-05-26 14:16:37 +0900

    refactor: [#4] Button의 cva의 default className에 py추가

--- src/components/Button/Button.cva.ts
+++ src/components/Button/Button.cva.ts
@@ -1,6 +1,7 @@
 import { cva, type VariantProps } from 'class-variance-authority'
 
 export type ButtonVariantProps = VariantProps<typeof buttonVariants>
+
 export const buttonVariants = cva(
   'h-[50px] w-[200px] inline-flex items-center justify-center rounded-md font-16r',
   {
@@ -13,8 +14,8 @@ export const buttonVariants = cva(
         strokeGreen: 'bg-white border border-main1 text-main1',
       },
       size: {
-        sm: 'w-[200px] px-[14px]',
-        lg: 'w-[320px] px-[14px]',
+        sm: 'w-[200px] py-[14px]',
+        lg: 'w-[320px] py-[14px]',
       },
     },
     defaultVariants: {
