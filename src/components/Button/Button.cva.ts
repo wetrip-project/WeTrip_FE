46a23e685c2e490556fff147bafefa762bbc0971
HANTAEDONG <htd0913@gmail.com>
2025-05-26 13:24:43 +0900

    feat: [#4] Button에 대한 cva구현

---
+++ src/components/Button/Button.cva.ts
@@ -0,0 +1,24 @@
+import { cva } from 'class-variance-authority'
+
+export const buttonVariants = cva(
+  'h-[50px] w-[200px] inline-flex items-center justify-center rounded-md font-16r',
+  {
+    variants: {
+      variant: {
+        activation: 'bg-main1 text-white',
+        deactivation: 'bg-disabled text-t3',
+        strokeGray1: 'bg-white border border-disabled text-t2',
+        strokeGray2: 'bg-white border border-t4 text-t1',
+        strokeGreen: 'bg-white border border-main1 text-main1',
+      },
+      size: {
+        sm: 'w-[200px] px-[14px]',
+        lg: 'w-[320px] px-[14px]',
+      },
+    },
+    defaultVariants: {
+      variant: 'activation',
+      size: 'sm',
+    },
+  },
+)
