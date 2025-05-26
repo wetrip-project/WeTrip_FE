237b1881100bbdece50420b91ec862a6cb10f2a5
HANTAEDONG <htd0913@gmail.com>
2025-05-26 14:17:44 +0900

    refactor: [#4] Button에 variants와 size props 필수로 받고 default세팅은 자동 완성되도록 수정

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,22 @@
-import { ButtonVariantProps, buttonVariants } from '@/components/Button/Button.cva'
+import { ButtonVariantProps, buttonVariants } from '@/components/Button/button.cva'
 import { cn } from '@/utils/cn'
 
-export interface ButtonProps extends ButtonVariantProps {
+type RequiredVariants = {
+  variant: NonNullable<ButtonVariantProps['variant']>
+  size: NonNullable<ButtonVariantProps['size']>
+}
+
+export interface ButtonProps
+  extends Omit<ButtonVariantProps, 'variant' | 'size'>,
+    RequiredVariants {
   children: React.ReactNode
   className?: string
   type?: 'button' | 'submit' | 'reset'
 }
 
-export const Button = ({ variant, size, children, className, type = 'button' }: ButtonProps) => {
+export const Button = ({ children, className, type = 'button', ...rest }: ButtonProps) => {
   return (
-    <button type={type} className={cn(buttonVariants({ variant, size }), className)}>
+    <button type={type} className={cn(buttonVariants(rest), className)}>
       {children}
     </button>
   )
