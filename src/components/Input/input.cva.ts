25215a2ee832b32d4b5d8a9855bd0040c5ad407e
HANTAEDONG <htd0913@gmail.com>
2025-05-28 15:08:59 +0900

    refactor: [#4] input의 cva 피그마에 맞게 수정

--- src/components/Input/input.cva.ts
+++ src/components/Input/input.cva.ts
@@ -1,7 +1,7 @@
 import { cva } from 'class-variance-authority'
 
 export const inputVariants = cva(
-  'w-[200px] h-[50px] py-[16px] px-[20px] border-stroke2 bg-white rounded-md font-16r',
+  'w-[320px] h-[50px] py-[16px] px-[20px] border-stroke2 bg-white rounded-md font-16r',
   {
     variants: {
       variant: {
