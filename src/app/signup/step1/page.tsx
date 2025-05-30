b8e6c090017d938eb8136bf56c25d98836be2335
HANTAEDONG <htd0913@gmail.com>
2025-05-30 20:56:50 +0900

    feat: [#4] 닉네임 설정페이지 구현

--- src/app/signup/step1/page.tsx
+++ src/app/signup/step1/page.tsx
@@ -1,6 +1,23 @@
-// 닉네임 설정
+import { Button } from '@/components/Button'
+import { Input } from '@/components/Input'
+
 const page = () => {
-  return <div>page</div>
+  return (
+    <div className='flex min-h-[calc(100vh-76px)] w-[320px] flex-col justify-between'>
+      <div className='flex flex-1 flex-col gap-4'>
+        <div className='font-20b'>사용할 닉네임을 적어주세요</div>
+        <Input variant='default' placeholder='한글,숫자 최대 10자' />
+      </div>
+      <div className='h-[90px] w-[320px] py-[14px]'>
+        <Button
+          children={'다음'}
+          variant={'deactivation'}
+          size={'lg'}
+          className='font-16r rounded-md'
+        />
+      </div>
+    </div>
+  )
 }
 
 export default page
