4eb896acb2eee971cdc6f42b68ea73ee7c8d8707
HANTAEDONG <htd0913@gmail.com>
2025-05-30 21:09:38 +0900

    feat: [#4] 프로필 완성 페이지 구현

--- src/app/signup/step5/page.tsx
+++ src/app/signup/step5/page.tsx
@@ -1,6 +1,27 @@
-// 프로필 완성
+import { Button } from '@/components/Button'
+import Image from 'next/image'
+import logoWithHappy from 'public/assets/icons/logoWithHappy.png'
+
 const page = () => {
-  return <div>page</div>
+  return (
+    <div className='relative min-h-[calc(100vh-76px)] w-[320px]'>
+      <div className='absolute top-[66px] left-[80px] h-[120px] w-[160px]'>
+        <Image src={logoWithHappy} alt={'d'} />
+      </div>
+      <div className='font-20b absolute top-[342px] left-0 flex w-[320px] justify-center'>
+        환영합니다 조조이님!
+        <br /> 위트립에서 즐거운 동행하세요
+      </div>
+      <div className='absolute bottom-0 left-0 h-[90px] w-[320px] py-[14px]'>
+        <Button
+          children={'다음'}
+          variant={'activation'}
+          size={'lg'}
+          className='font-16r rounded-md'
+        />
+      </div>
+    </div>
+  )
 }
 
 export default page
