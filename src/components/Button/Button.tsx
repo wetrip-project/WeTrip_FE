eefd22fe23fa9a8824d604391881042b3780f8d9
HANTAEDONG <htd0913@gmail.com>
2025-05-30 15:55:41 +0900

    refactor: [#4] Button에 onclick 받도록  구현

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,9 +2,9 @@ import { buttonVariants } from '@/components/Button/button.cva'
 import { cn } from '@/utils/cn'
 import { ButtonProps } from './button.types'
 
-export const Button = ({ children, className, type = 'button', ...rest }: ButtonProps) => {
+export const Button = ({ children, className, type = 'button', onClick, ...rest }: ButtonProps) => {
   return (
-    <button type={type} className={cn(buttonVariants(rest), className)}>
+    <button type={type} className={cn(buttonVariants(rest), className)} onClick={onClick}>
       {children}
     </button>
   )
