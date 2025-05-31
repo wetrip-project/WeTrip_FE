c8fca92b1e602f2fe552a18a3fd828ce68e77562
HANTAEDONG <htd0913@gmail.com>
2025-05-31 18:33:43 +0900

    refactor: [#4] Options 분리

---
+++ src/constants/options/ageOptions.ts
@@ -0,0 +1,12 @@
+const ageOptions = [
+  { label: '10대', value: '10' },
+  { label: '20대', value: '20' },
+  { label: '30대', value: '30' },
+  { label: '40대', value: '40' },
+  { label: '50대', value: '50' },
+  { label: '60대', value: '60' },
+  { label: '70대', value: '70' },
+  { label: '80대', value: '80' },
+]
+
+export default ageOptions
