0a9720f11ff8859d6b87487060d50a20c59a5b8d
JangGusWjd <jjanghj3624@gmail.com>
2025-06-01 20:32:39 +0900

    feat: [#9] 동행글 유무에 따라 메인 컴포넌트 분기 렌더링 구현

---
+++ src/app/main/page.tsx
@@ -0,0 +1,28 @@
+import { Search } from '@/components/Search'
+import NoPostPrompt from 'src/features/main/components/NoPostPrompt'
+import PostOverview from 'src/features/main/components/PostOverview'
+import PopularPosts from 'src/features/main/components/PopularPosts'
+import RelatedPosts from 'src/features/main/components/RelatedPosts'
+
+const Page = () => {
+  const hasPosts = false // TODO : API 데이터 연결시 상태 변경 예정 반영
+
+  const handleSearch = (value: string) => {
+    console.log('검색어:', value)
+    // 검색 API 호출
+  }
+
+  return (
+    <div className='flex min-h-[calc(100vh-62px)] w-[360px] flex-col justify-between pt-[8px]'>
+      <div className='mb-5 flex flex-col gap-[16px] px-5'>
+        <Search onSearch={handleSearch} placeholder='동행 글 검색' />
+
+        {hasPosts ? <PostOverview /> : <NoPostPrompt />}
+      </div>
+
+      {hasPosts ? <RelatedPosts /> : <PopularPosts />}
+    </div>
+  )
+}
+
+export default Page
