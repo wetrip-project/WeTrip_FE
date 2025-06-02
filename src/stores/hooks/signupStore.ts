0124b03bbf8d5bdb710959558746cbca5a814e91
HANTAEDONG <htd0913@gmail.com>
2025-06-02 14:15:55 +0900

    feat: [#12] signUpStore 구현

---
+++ src/stores/hooks/signupStore.ts
@@ -0,0 +1,11 @@
+import { create } from 'zustand'
+
+type SignupStore = {
+  step: number
+  setStep: (step: number) => void
+}
+
+export const useSignupStore = create<SignupStore>((set) => ({
+  step: 1,
+  setStep: (step) => set({ step }),
+}))
