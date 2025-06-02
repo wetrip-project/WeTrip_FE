81e2627a8067064b95856683cfbdf821e3027b3b
HANTAEDONG <htd0913@gmail.com>
2025-06-02 15:41:53 +0900

    refactor: [#12] 뒤로가기를 pushState로 수정

--- src/app/signup/layout.tsx
+++ src/app/signup/layout.tsx
@@ -1,15 +1,42 @@
 'use client'
+
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 import Icon from '@/components/Icon/icon'
 import SignupScaffold from 'src/features/auth/components/SignupScaffold'
 
-export default function signupLayout({
+const Layout = ({
   children,
 }: Readonly<{
   children: React.ReactNode
-}>) {
+}>) => {
+  const router = useRouter()
+  const [step, setStep] = useState(1)
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    setStep(Number(params.get('step') || '1'))
+    const handler = () => {
+      const params = new URLSearchParams(window.location.search)
+      setStep(Number(params.get('step') || '1'))
+    }
+    window.addEventListener('popstate', handler)
+    return () => window.removeEventListener('popstate', handler)
+  }, [])
+  const goPrevStep = () => {
+    if (step > 1) {
+      const newSearch = new URLSearchParams(window.location.search)
+      newSearch.set('step', String(step - 1))
+      const newUrl = `/signup?${newSearch.toString()}`
+      window.history.pushState({}, '', newUrl)
+      setStep(step - 1)
+    } else {
+      router.push('/login')
+    }
+  }
+
   return (
     <div className='h-[100svh] w-[360px] px-5'>
-      <div className='w-[320px] py-3' onClick={() => window.history.back()}>
+      <div className='w-[320px] py-3' onClick={goPrevStep}>
         <Icon iconName={'Back'} />
       </div>
       <SignupScaffold />
@@ -17,3 +44,5 @@ export default function signupLayout({
     </div>
   )
 }
+
+export default Layout
