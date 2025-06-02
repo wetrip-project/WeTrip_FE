b13afcb83dc0b4e4322949b0b8121773efb96013
HANTAEDONG <htd0913@gmail.com>
2025-06-02 14:49:02 +0900

    refactor: [#12] signup layout의 돌아가기 아이콘에 history.back 추가

--- src/app/signup/layout.tsx
+++ src/app/signup/layout.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Icon from '@/components/Icon/icon'
 import SignupScaffold from 'src/features/auth/components/SignupScaffold'
 
@@ -8,11 +9,11 @@ export default function signupLayout({
 }>) {
   return (
     <div className='h-[100svh] w-[360px] px-5'>
-      <div className='w-[320px] py-3'>
+      <div className='w-[320px] py-3' onClick={() => window.history.back()}>
         <Icon iconName={'Back'} />
       </div>
       <SignupScaffold />
-      <div className='min-h-[calc(100vh-62px)] w-[320px] flex-col justify-between'>{children}</div>
+      <div className='min-h-[calc(100vh-62px)] w-[320px]'>{children}</div>
     </div>
   )
 }
