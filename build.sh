c5d6a9636d80bced6f7969bd6697d9d5049456e9
HANTAEDONG <htd0913@gmail.com>
2025-06-12 13:39:09 +0900

    refactor: [#12] build.sh 파일 오류 해결

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
-#!/bin/sh
-
-# 실행 방법: ./build.sh
-
-cd ..
-mkdir -p output
-
-# WeTrip_FE의 전체 파일을 output으로 복사
-cp -R ./WeTrip_FE/* ./output
-
-# 복사한 output 내용을 다시 WeTrip_FE로 덮어쓰기
-cp -R ./output/* ./WeTrip_FE
+cd ../
+mkdir output
+cp -R ./team-repository-name/* ./output
+cp -R ./output ./team-repository-name/
