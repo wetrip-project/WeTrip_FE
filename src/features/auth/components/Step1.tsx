d6b8b4ceac8d1010d995cb6aeefa10b7c9b355dc
HANTAEDONG <htd0913@gmail.com>
2025-07-07 15:47:52 +0900

    refactor: [#1] Step1 컴포넌트에서 버튼을 motion.button으로 변경하고 스타일 수정

--- src/features/auth/components/Step1.tsx
+++ src/features/auth/components/Step1.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Button } from '@/components/Button'
 import { Input } from '@/components/Input'
+import { motion } from 'motion/react'
 
 type Props = {
   onNext: () => void
@@ -11,16 +12,19 @@ const Step1 = ({ onNext }: Props) => {
     <div className='flex h-[calc(100vh-62px)] w-[320px] flex-col justify-between'>
       <div className='flex flex-1 flex-col gap-4'>
         <div className='font-20b'>사용할 닉네임을 적어주세요</div>
-        <Input variant='default' placeholder='한글,숫자 최대 10자' />
+        <Input
+          variant='default'
+          placeholder='한글,숫자 최대 10자'
+          className='focus:outline-main1'
+        />
       </div>
       <div className='h-[90px] w-[320px] py-[14px]'>
-        <Button
-          children={'다음'}
-          variant={'deactivation'}
-          size={'lg'}
-          className='font-16r rounded-md'
-          onClick={onNext}
-        />
+        <motion.button
+          className='bg-main1 font-16r inline-flex w-[320px] items-center justify-center rounded-md py-[14px] text-white'
+          whileTap={{ scale: 0.95 }}
+        >
+          다음
+        </motion.button>
       </div>
     </div>
   )
