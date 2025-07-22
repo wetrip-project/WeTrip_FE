a30aa18d1550487585b2b45293f2085d32e7764a
HANTAEDONG <htd0913@gmail.com>
2025-07-22 18:31:46 +0900

    feat: [#22] Kakao Button에 라우팅 도입

--- src/features/auth/components/KaKaoLoginButton.tsx
+++ src/features/auth/components/KaKaoLoginButton.tsx
@@ -1,18 +1,24 @@
+'use client'
+
 import { Button } from '@/components/Button'
 import { KaKaoLogo } from '@/components/Icon'
+import { useRouter } from 'next/navigation'
 
 const KaKaoLoginButton = () => {
+  const router = useRouter()
   return (
     <Button
-      children={
-        <div className='flex h-[24px] gap-1 align-middle'>
-          <KaKaoLogo />
-          카카오톡으로 시작하기
-        </div>
-      }
-      size={'lg'}
+      size='lg'
       className='font-16r bg-[#FAE100] text-black'
-    />
+      onClick={() =>
+        router.push(`${process.env.NEXT_PUBLIC_BASE_SERVER_URL}/oauth2/authorization/kakao`)
+      }
+    >
+      <div className='flex h-[24px] gap-1 align-middle'>
+        <KaKaoLogo />
+        카카오톡으로 시작하기
+      </div>
+    </Button>
   )
 }
 
