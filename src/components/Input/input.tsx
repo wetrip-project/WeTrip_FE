ab01a92692db2fdc284c2a1b757dea2bc535b5ad
HANTAEDONG <htd0913@gmail.com>
2025-07-25 17:06:08 +0900

    refactor: [#22] input에 타입 받도록 수정

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -2,6 +2,8 @@ import { inputVariants } from './input.cva'
 import { cn } from '@/utils/cn'
 import { InputProps } from './input.types'
 
-export const Input = ({ variant = 'default', className, ...props }: InputProps) => {
-  return <input className={cn(inputVariants({ variant }), className)} {...props} />
+export const Input = ({ variant = 'default', className, onChange, ...props }: InputProps) => {
+  return (
+    <input className={cn(inputVariants({ variant }), className)} {...props} onChange={onChange} />
+  )
 }
--- src/components/Input/input.types.ts
+++ src/components/Input/input.types.ts
@@ -3,4 +3,5 @@ import { InputHTMLAttributes } from 'react'
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   variant?: 'default' | 'sub'
   className?: string
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
