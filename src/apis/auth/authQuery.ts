ac8dbb3775a931856b6b221fb6b056023d7c3bcd
HANTAEDONG <htd0913@gmail.com>
2025-07-26 08:59:35 +0900

    refactor: [#22] nickname 변경시 재요청위해 querykey에 nickname 추가

--- src/apis/auth/authQuery.ts
+++ src/apis/auth/authQuery.ts
@@ -9,7 +9,7 @@ export interface NicknameSetResponse {
 
 export const useSetNickname = (nickname: string): UseQueryResult<NicknameSetResponse, Error> => {
   return useQuery<NicknameSetResponse, Error>({
-    queryKey: [querykeys.userOnboarding.nickname],
+    queryKey: [querykeys.userOnboarding.nickname, nickname],
     queryFn: () => authService.postNickname(nickname),
     enabled: !!nickname,
   })
