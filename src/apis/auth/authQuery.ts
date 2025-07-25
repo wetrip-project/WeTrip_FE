6739e1b796d9939ac68f5aab3a478c7b6dfe69d7
HANTAEDONG <htd0913@gmail.com>
2025-07-25 17:04:09 +0900

    refactor: [#22] authQuery와 Service에 타입 지정하도록 수정

--- src/apis/auth/authQuery.ts
+++ src/apis/auth/authQuery.ts
@@ -1,10 +1,16 @@
-import { useQuery } from '@tanstack/react-query'
+import { UseQueryResult, useQuery } from '@tanstack/react-query'
 import { querykeys } from '../querykeys'
 import { authService } from './authService'
 
-export const useSetNickname = (nickname: string) => {
-  return useQuery({
+export interface NicknameSetResponse {
+  nickname: string
+  message: string
+}
+
+export const useSetNickname = (nickname: string): UseQueryResult<NicknameSetResponse, Error> => {
+  return useQuery<NicknameSetResponse, Error>({
     queryKey: [querykeys.userOnboarding.nickname],
     queryFn: () => authService.postNickname(nickname),
+    enabled: !!nickname,
   })
 }
--- src/apis/auth/authService.ts
+++ src/apis/auth/authService.ts
@@ -1,11 +1,11 @@
 import { apiClient } from '@/utils/ApiClient'
+import { NicknameSetResponse } from './authQuery'
 
 class AuthService {
   async postNickname(nickName: string) {
-    return await apiClient.post('/api/auth/signup/nickname', {
+    return await apiClient.post<NicknameSetResponse>('/api/auth/signup/nickname', {
       name: nickName,
     })
   }
 }
-
 export const authService = new AuthService()
