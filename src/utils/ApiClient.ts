50fe875493a83e44711c96fa36fa9ea1b788ed3c
HANTAEDONG <htd0913@gmail.com>
2025-07-26 16:19:37 +0900

    feat: [#22] HTTPclient와 apiClient 통합

--- src/utils/ApiClient.ts
+++
@@ -1,5 +0,0 @@
-import HttpClient from './HttpClient'
-
-const BASE_URL = 'https://dev-wetrip.shop/'
-
-export const apiClient = new HttpClient(BASE_URL)
--- src/utils/HttpClient.ts
+++ src/utils/HttpClient.ts
@@ -1,26 +1,25 @@
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
 
-export default class HttpClient {
+class HttpClient {
   private readonly client: AxiosInstance
-
-  constructor(baseURL: string) {
+  private readonly BASE_URL =
+    process.env.NEXT_PUBLIC_BASE_SERVER_URL || 'https://dev-wetrip.shop/api'
+  constructor() {
     this.client = axios.create({
-      baseURL,
+      baseURL: this.BASE_URL,
       // withCredentials: true,
       timeout: 2000,
     })
-
     this.client.interceptors.request.use((config) => {
       // const token = localStorage.getItem('accessToken')
       const token =
-        'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2IiwiaWF0IjoxNzUzNDQ2NjQ1LCJleHAiOjE3NTM0NDg0NDV9._f7gElBS6QvrqGTotu_xgaxtaNMAr_0KEeMiMmM9lGg'
+        'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2IiwiaWF0IjoxNzUzNTEzOTkzLCJleHAiOjE3NTM1MjExOTN9.GCa_6rJa7Z8zX_zyLbQST_aGSdOxAAmMhI6Svs7VQNM'
       if (token) {
         config.headers.Authorization = `Bearer ${token}`
       }
       return config
     })
   }
-
   public async get<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<T> {
     const response: AxiosResponse<T> = await this.client.get(url, config)
     return response.data
@@ -33,7 +32,6 @@ export default class HttpClient {
     const response: AxiosResponse<T> = await this.client.post(url, data, config)
     return response.data
   }
-
   public async put<T = unknown, D = unknown>(
     url: string,
     data?: D,
@@ -42,9 +40,12 @@ export default class HttpClient {
     const response: AxiosResponse<T> = await this.client.put(url, data, config)
     return response.data
   }
-
   public async delete<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<T> {
     const response: AxiosResponse<T> = await this.client.delete(url, config)
     return response.data
   }
 }
+
+const apiClient = new HttpClient()
+
+export default apiClient
