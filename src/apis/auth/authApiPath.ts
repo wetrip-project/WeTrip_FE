9706b2d6287231de9f6e464fde5d2fba77790130
HANTAEDONG <htd0913@gmail.com>
2025-07-26 15:38:40 +0900

    feat: [#22] auth에 대한 ApiPath 파일 구현

---
+++ src/apis/auth/authApiPath.ts
@@ -0,0 +1,10 @@
+export const AUTH_SIGNUP_PREFIX = '/api/auth/signup' as const
+
+export const authApiPath = {
+  profile: AUTH_SIGNUP_PREFIX,
+  urlIssuance: `${AUTH_SIGNUP_PREFIX}/profile_photo_url`,
+  tripType: `${AUTH_SIGNUP_PREFIX}/trip_type`,
+  nickname: `${AUTH_SIGNUP_PREFIX}/nickname`,
+  genderAge: `${AUTH_SIGNUP_PREFIX}/gender_age`,
+  complete: `${AUTH_SIGNUP_PREFIX}/complete`,
+} as const
