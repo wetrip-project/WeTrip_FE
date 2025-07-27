28b8bccf27ce58032d43d4fe4886d806e2f8cc12
HANTAEDONG <htd0913@gmail.com>
2025-07-27 14:00:05 +0900

    feat: [#22] genderAge에 대한 querykey 추가

--- src/apis/querykeys.ts
+++ src/apis/querykeys.ts
@@ -2,5 +2,6 @@ export const querykeys = {
   userOnboarding: {
     nickname: () => ['nickname'],
     profilePhoto: () => ['profilePhoto'],
+    genderAge: ({ age, gender }: { age: number; gender: string }) => ['genderAge', age, gender],
   },
 }
