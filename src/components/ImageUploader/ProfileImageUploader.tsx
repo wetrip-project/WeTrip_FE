5fea76b701be60a5ddfdf186e66619937661de49
HANTAEDONG <htd0913@gmail.com>
2025-07-28 14:10:08 +0900

    remove: [#22] ProfileImageUploader로 파일명 수정

---
+++ src/components/ImageUploader/ProfileImageUploader.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { UploadImg } from '../Icon'
+import defaultImg from 'public/assets/icons/defaultAvator.png'
+
+type Props = {
+  preview: string
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const ProfileImageUploader = ({ preview, handleChange }: Props) => {
+  const imageSrc = preview || defaultImg.src
+
+  return (
+    <div className='relative h-[145px] w-[137px]'>
+      <img
+        src={imageSrc}
+        alt='preview'
+        className='absolute top-0 left-0 h-[137px] w-[137px] rounded-full object-cover'
+      />
+      <div className='absolute right-0 bottom-0 h-[48px] w-[48px]'>
+        <label className='relative block h-full w-full cursor-pointer'>
+          <UploadImg />
+          <input
+            type='file'
+            id='avator'
+            accept='image/png, image/jpeg'
+            multiple={false}
+            onChange={handleChange}
+            className='absolute top-0 left-0 h-full w-full opacity-0'
+          />
+        </label>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileImageUploader
