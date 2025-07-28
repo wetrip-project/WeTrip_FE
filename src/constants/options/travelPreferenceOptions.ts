b38434ac05afe8cb46d52d20cbf44f4688a7c5fd
HANTAEDONG <htd0913@gmail.com>
2025-07-28 15:19:40 +0900

    add: [#22] 여행 취향 상수 추가

---
+++ src/constants/options/travelPreferenceOptions.ts
@@ -0,0 +1,12 @@
+const travelPreferenceOptions = [
+  { label: '맛집', value: 0 },
+  { label: '쇼핑', value: 1 },
+  { label: '관광', value: 2 },
+  { label: '사진', value: 3 },
+  { label: '액티비티', value: 4 },
+  { label: '술/파티', value: 5 },
+  { label: '힐링', value: 6 },
+  { label: '친목', value: 7 },
+] as const
+
+export default travelPreferenceOptions
